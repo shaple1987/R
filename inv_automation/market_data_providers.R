695e516d573bd4466b7a7465cfcff6e959048e62
shaple1987 <shaple1987@gmail.com>
2026-07-18 19:43:54 -0400

    update dividend data src

--- inv_automation/market_data_providers.R
+++ inv_automation/market_data_providers.R
@@ -7,19 +7,49 @@ library(rvest)
 
 HOLIDAY_CALENDAR <- read.delim("~/Hai/Code/R/global_config/holidays.txt", header = FALSE, colClasses = "Date")$V1
 
-extract_div <- function(ticker, start_date = NULL){
-  message(sprintf("Getting div history for %s", ticker))
-  doc <- rvest::read_html(sprintf("https://www.dividendinformation.com/search_ticker/?identifier=%s", ticker))
-  tables <- doc %>% rvest::html_elements("table") %>% rvest::html_table(header = TRUE)
-  output <- tables[4] %>% as.data.frame
-  output$Date <- as.Date(output$Date)
-  output$Amount.Per.Share <- as.numeric(sub("\\$", "", output$Amount.Per.Share))
-  output$Ticker <- ticker
-  output$Type <- "CASH"
-  if(!is.null(start_date)) output <- subset(output, Date >= start_date)
+extract_div <- function(ticker, start_date = NULL,
+                        src=c("divinfo","divhist")[2]){
+  valid_srcs <- c("divhist")
+  message(sprintf("Getting div history for %s from source %s", ticker, src
+                  ))
+  if(src=='divinfo'){ # no longer working as of Oct 20, 2025
+    doc <- rvest::read_html(sprintf("https://www.dividendinformation.com/search_ticker/?identifier=%s", ticker))
+    tables <- doc %>% rvest::html_elements("table") %>% rvest::html_table(header = TRUE)
+    output <- tables[4] %>% as.data.frame
+    output$Ex.Date <- as.Date(output$Date)
+    output$Amount.Per.Share <- as.numeric(sub("\\$", "", output$Amount.Per.Share))
+    output$Ticker <- ticker
+    output$Type <- "CASH"
+    output <- output[, c("Ticker", "Ex.Date", "Type", "Amount.Per.Share")]
+  }else if(src=='divhist'){
+    doc <- rvest::read_html(sprintf("https://dividendhistory.org/payout/%s/", ticker))
+    tables <- doc %>% rvest::html_elements("table") %>% rvest::html_table(header = TRUE)
+    found <- F
+    idx<-0
+    while(found==F){
+      idx <- idx + 1
+      first_col <- names(tables[idx][[1]])[1]
+      if(first_col== "Ex-Dividend Date") found <- T
+    }
+    output <- tables[idx][[1]] %>% as.data.frame
+    # remove estimated divs
+    names(output)[length(names(output))] <- "pct_chg" # standardize column name
+    output <- subset(output, pct_chg != "unconfirmed/estimated")
+    output$Ticker <- ticker
+    output$Amount.Per.Share <-as.numeric(sub("\\$", "", output$`Cash Amount`))
+    output$Ex.Date <- as.Date(output$`Ex-Dividend Date`)
+    output$Type <- 'CASH'
+    output$Payment.Date <- as.Date(output$`Payout Date`)
+    output <- output[,c("Ticker","Ex.Date","Type","Amount.Per.Share","Payment.Date")]
+  }else{
+    print(sprintf("error! %s is not a supported data source, pick one of: %s", src, paste(valid_srcs, collapse=', ')))
+    return(NA)
+  }
+  if(!is.null(start_date)) output <- subset(output, Ex.Date >= start_date)
   print(sprintf("%d rows returned", nrow(output)))
-  return(output[, c("Ticker", "Date", "Type", "Amount.Per.Share")])
+  return(output)
 }
+
 
 get_prices_by_date<-function(tickers, sdate){
   out<-data.frame(ticker=tickers,date=sdate,close=NA)
@@ -69,8 +99,8 @@ get_prices_by_date_range<-function(tickers, sdate, edate){
 update_div_history <- function(){
   # Load list of div stocks
   div_list<-rbind(
-    read_excel("~/Hai/Spreadsheets/inv_automation/Inv Workbook.xlsx",sheet = "Div History",range = "J1:O13"),
-    read_excel("~/Hai/Spreadsheets/inv_automation/Inv Workbook.xlsx",sheet = "Div History",range = "S1:X12")
+    read_excel("~/Hai/inv_automation/Inv Workbook.xlsx",sheet = "Div History",range = "J1:R13"),
+    read_excel("~/Hai/inv_automation/Inv Workbook.xlsx",sheet = "Div History",range = "S1:AA12")
   )
   tickers<-div_list$Ticker
   excl_tickers<-c("UBA","GIGE")
